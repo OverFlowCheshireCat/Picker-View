cf016084254419fc536627d90df06ecc5f84a5cd
dit03 <dit@imac-4.local>
2019-04-18 16:40:19 +0900

    New Source

--- Picker View/ViewController.swift
+++ Picker View/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var data: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var colorList = ["Red", "Blue", "Yellow"]
+    var animalList = ["Dog", "Cat", "Bear", "Tiger", "Rion"]
+    
+    var lastSelColor = 0, lastSelAnimal = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        data.text = "Red Dog"
     }
-
-
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (component == 0) {
+            return colorList.count
+        } else {
+            return animalList.count
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (component == 0) {
+            return colorList[row]
+        } else {
+            return animalList[row]
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (component == 0) {
+            data.text = colorList[row] + " " + animalList[lastSelAnimal]
+            lastSelColor = row
+        } else {
+            data.text = colorList[lastSelColor] + " " + animalList[row]
+            lastSelAnimal = row
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
 }
-
